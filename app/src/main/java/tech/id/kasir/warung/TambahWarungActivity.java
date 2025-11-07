70382665e22269fbe30f9cf2eb065c12bc2d0465
febriwinando <35925865+febriwinando@users.noreply.github.com>
2025-11-07 14:31:38 +0700

    Penambahan Toko

--- app/src/main/java/tech/id/kasir/warung/TambahWarungActivity.java
+++ app/src/main/java/tech/id/kasir/warung/TambahWarungActivity.java
@@ -1,6 +1,7 @@
 package tech.id.kasir.warung;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +9,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 import tech.id.kasir.R;
 
 public class TambahWarungActivity extends AppCompatActivity {
 
+    TextInputEditText tietNamaToko, tietOwnerToko, tietKontakToko, tietAlamatToko, tietKelurahanToko, tietKecamatanToko, tietKotaToko, tietProvinsiToko, tietKodePosToko;
+    ImageButton ibSaveInfoWarung;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +27,20 @@ public class TambahWarungActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        tietNamaToko = findViewById(R.id.tietNamaToko);
+        tietOwnerToko = findViewById(R.id.tietOwnerToko);
+        tietKontakToko = findViewById(R.id.tietKontakToko);
+        tietAlamatToko = findViewById(R.id.tietAlamatToko);
+        tietKelurahanToko = findViewById(R.id.tietKelurahanToko);
+        tietKecamatanToko = findViewById(R.id.tietKecamatanToko);
+        tietKotaToko = findViewById(R.id.tietKotaToko);
+        tietProvinsiToko = findViewById(R.id.tietProvinsiToko);
+        tietKodePosToko = findViewById(R.id.tietKodePosToko);
+        ibSaveInfoWarung = findViewById(R.id.ibSaveInfoWarung);
+
+
+
     }
 }
